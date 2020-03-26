fe476e106b553364645a16ccfda7c7131b19f804
Alex Canessa <canessa.alex@gmail.com>
2020-03-26 15:19:29 +0100

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  plugins: ["jest"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off"
+  },
+  overrides: [
+    {
+      files: "*.test.tsx",
+      env: {
+        "jest/globals": true
+      }
+    }
+  ]
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# {{ Name of the project }}
+
+Check [Standard Readme](https://github.com/RichardLitt/standard-readme) when writing this file.
